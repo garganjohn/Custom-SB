eb1e8ca3f292ea322211aa752624d3ba9fb7f32c
garganjohn <garganjohn@gmail.com>
2019-07-29 19:27:06 -0400

    added first sound

--- app/src/main/java/org/pursuit/custom_sb/MainActivity.java
+++ app/src/main/java/org/pursuit/custom_sb/MainActivity.java
@@ -1,13 +1,41 @@
 package org.pursuit.custom_sb;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonZero;
+    private MediaPlayer mpZero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    void createButtonZero() {
+        buttonZero = findViewById(R.id.sound_btn_0);
+        buttonZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mpZero = MediaPlayer.create(getApplicationContext(), R.raw.kyle_guy);
+                mpZero.setVolume(100f, 100f);
+                mpZero.start();
+                mpZero.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

8de58a447c0d49635d11884cbeebf26d036f5fdd
garganjohn <garganjohn@gmail.com>
2019-08-04 14:41:31 -0400

    added recyclerview and more sounds

--- app/src/main/java/org/pursuit/custom_sb/MainActivity.java
+++ app/src/main/java/org/pursuit/custom_sb/MainActivity.java
@@ -4,41 +4,64 @@ import android.media.MediaPlayer;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.pursuit.custom_sb.RecyclerView.SoundAdapter;
+
+import java.lang.reflect.Field;
+
 public class MainActivity extends AppCompatActivity {
-    private Button buttonZero;
-    private MediaPlayer mpZero;
+    private RecyclerView soundRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CustomSound customSound = new CustomSound(this);
-        CustomSoundButton soundZero = new CustomSoundButton(this, 0, customSound);
-        ConstraintLayout container = findViewById(R.id.main_containter);
-        container.addView(soundZero, 1);
+        initRecyclerView();
     }
 
-    void createButtonZero() {
-        buttonZero = findViewById(R.id.sound_btn_0);
-        buttonZero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mpZero = MediaPlayer.create(getApplicationContext(), R.raw.kyle_guy);
-                mpZero.setVolume(100f, 100f);
-                mpZero.start();
-                mpZero.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.release();
-                    }
-                });
-                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
+    private void initRecyclerView() {
+        soundRecyclerView = findViewById(R.id.sound_recyclerview);
+        soundRecyclerView.setAdapter(new SoundAdapter(getAllRawResources()));
+        soundRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
     }
+
+    private String[] getAllRawResources() {
+        Field fields[] = R.raw.class.getDeclaredFields();
+        String[] names = new String[fields.length];
+
+        try {
+            for (int i = 0; i < fields.length; i++) {
+                Field f = fields[i];
+                names[i] = f.getName();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return names;
+    }
+
+//    void createButtonZero() {
+//        buttonZero = findViewById(R.id.sound_btn_0);
+//        buttonZero.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mpZero = MediaPlayer.create(getApplicationContext(), R.raw.kyle_guy);
+//                mpZero.setVolume(100f, 100f);
+//                mpZero.start();
+//                mpZero.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                    @Override
+//                    public void onCompletion(MediaPlayer mp) {
+//                        mp.release();
+//                    }
+//                });
+//                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
 }
--- app/src/main/java/org/pursuit/custom_sb/RecyclerView/SoundViewHolder.java
+++ app/src/main/java/org/pursuit/custom_sb/RecyclerView/SoundViewHolder.java
@@ -3,12 +3,23 @@ package org.pursuit.custom_sb.RecyclerView;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+
+import org.pursuit.custom_sb.R;
 
 public class SoundViewHolder extends RecyclerView.ViewHolder {
+    private Button soundButton;
     public SoundViewHolder(@NonNull View itemView) {
         super(itemView);
+        soundButton = itemView.findViewById(R.id.sound_button);
     }
     void onBind(){
         //TODO
+        soundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
